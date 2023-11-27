50bf17920fef24e78a315ba218aa253b62f8c1b2
Haggai Yechiel <89970921+Haggai33@users.noreply.github.com>
2023-11-27 18:55:50 +0200

    7.0
    Spotify Artist Scraper and Follower
    Overview
    This script is designed to interact with Spotify playlists, scrape artist information, check for Instagram accounts, and optionally follow artists on Spotify. It integrates with Google Sheets to cross-reference data and can export results to CSV and text files.
    
    Features
    Scrape Spotify Playlist: Extracts artist information from a given Spotify playlist URL.
    Instagram Account Search: Searches for Instagram accounts of artists and checks if they are already in a Google Sheet database.
    Follow Artists on Spotify: Offers an option to follow artists from the playlist on Spotify.
    Export Data: Saves the scraped data to CSV and text files, including a summary of found Instagram accounts and newly followed artists on Spotify.
    Usage Scenarios
    Extracting Artist Data: Input a Spotify playlist URL to get artist names and their Spotify URLs.
    Instagram Account Search: The script scrapes each artist's Spotify page for an Instagram account link. If not found, it checks a Google Sheet database for a match.
    Following Artists: You can choose to follow artists from the playlist who are not already followed on Spotify.
    Data Export: After processing, you have the option to save the data in CSV or text format. The text file includes a summary of total artists, found Instagram accounts, accounts found in the database, and accounts not found. It also lists newly followed artists on Spotify if that option was chosen.
    Requirements
    Python environment with necessary libraries (spotipy, selenium, gspread, etc.).
    Spotify API credentials.
    Google Sheets API setup with a service account.
    Chrome WebDriver for Selenium.
    Note
    Ensure all credentials and URLs are correctly set up in the script before running it. The script is interactive and will prompt for inputs and choices during execution.

--- IGExt.py
+++ IGExt.py
@@ -8,14 +8,25 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 import time
 import csv
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 # Spotify API credentials
-CLIENT_ID = '***'
-CLIENT_SECRET = '****'
+CLIENT_ID = 'f8ad5b8eb68940b48b5dac24e5567e2b'
+CLIENT_SECRET = 'e2966868aae44b078cb89b4f01577709'
 REDIRECT_URI = 'http://localhost:8888/callback'
 
 # Initialize Spotify API client
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope='playlist-read-private'))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope='playlist-read-private user-follow-read user-follow-modify'))
+
+# Google Sheets setup
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name('C:\\Aum.Music\\credentials.json', scope)
+client = gspread.authorize(creds)
+spreadsheet_url = "https://docs.google.com/spreadsheets/d/1j8jRlbBJi_6GREbj9vhlUvv6SQXAFkZp32M4WLy0Wkk"
+sheet = client.open_by_url(spreadsheet_url).worksheet("IG Artist")
+
 
 def get_playlist_artists(sp, playlist_url):
     playlist_id = playlist_url.split('/')[-1].split('?')[0]
@@ -45,6 +56,35 @@ def smooth_scroll(driver, class_name):
             return True
     return False
 
+def check_in_db(username, artist_name):
+    try:
+        ig_user_col = sheet.col_values(4)  # IG user column
+        artist_name_col = sheet.col_values(1)  # Artist Name column
+    except:
+        return "No"
+
+    # Check if username is in IG user column
+    if username in ig_user_col:
+        return "Yes"
+
+    # Function to try different name formats
+    def try_name_formats(name):
+        formats = [
+            name.lower(),  # All lowercase
+            name.title(),  # First letter of each word capitalized
+            name.upper(),  # All uppercase
+            ' '.join([name.split()[0].capitalize(), name.split()[1].lower()]) if len(name.split()) > 1 else name  # First name capitalized, last name lowercase
+        ]
+        for fmt in formats:
+            if fmt in artist_name_col:
+                index = artist_name_col.index(fmt)
+                return sheet.cell(index + 1, 4).value or "No"
+        return "No"
+
+    return try_name_formats(artist_name)
+
+
+
 def scrape_artist_pages(driver, artists_info):
     instagram_accounts = {}
     for name, spotify_url in artists_info:
@@ -55,16 +95,21 @@ def scrape_artist_pages(driver, artists_info):
                 instagram_link = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.oe0FHRJU7PvjoTnXJmfr[href*="instagram.com"]')))
                 instagram_url = instagram_link.get_attribute('href')
                 instagram_username = instagram_url.split('instagram.com/')[-1].split('/')[0].split('?')[0]
-                print(f"{name}: {instagram_username}")
-                key = (name, instagram_username)
+                in_db = check_in_db(instagram_username, name)
+                if in_db == "No":
+                    instagram_username = "NONE"
+                print(f"{name}: {instagram_username} (In DB: {in_db})")
+                key = (name, instagram_username, in_db)
             except (NoSuchElementException, TimeoutException):
-                print(f"{name}: None")
-                key = (name, "NONE")
+                in_db = check_in_db("NONE", name)
+                print(f"{name}: None (In DB: {in_db})")
+                key = (name, "NONE", in_db)
 
             instagram_accounts[key] = instagram_accounts.get(key, 0) + 1
         else:
-            print(f"{name}: None")
-            key = (name, "NONE")
+            in_db = check_in_db("NONE", name)
+            print(f"{name}: None (In DB: {in_db})")
+            key = (name, "NONE", in_db)
             instagram_accounts[key] = instagram_accounts.get(key, 0) + 1
 
     return instagram_accounts
@@ -72,10 +117,27 @@ def scrape_artist_pages(driver, artists_info):
 def save_to_csv(instagram_accounts):
     with open('C:\\Users\\Administrator\\Downloads\\instagram_accounts.csv', mode='w', newline='', encoding='utf-8-sig') as file:
         writer = csv.writer(file)
-        writer.writerow(['Artist Name', 'Instagram Username', 'Count'])
-        for (name, username), count in instagram_accounts.items():
-            writer.writerow([name, username, count])
+        writer.writerow(['Artist Name', 'Instagram Username', 'Count', 'In DB'])
+        for (name, username, in_db), count in instagram_accounts.items():
+            writer.writerow([name, username, count, in_db])
 
+def save_to_text(instagram_accounts, new_followed_artists=None):
+    with open('C:\\Users\\Administrator\\Downloads\\instagram_accounts.txt', mode='w', encoding='utf-8') as file:
+        total_artists = len(instagram_accounts)
+        found = sum(1 for _, _, in_db in instagram_accounts if in_db == "Yes")
+        found_in_db = sum(1 for _, _, in_db in instagram_accounts if in_db != 'No' and in_db != 'Yes')
+        not_found = total_artists - found - found_in_db
+        for (name, username, in_db), count in instagram_accounts.items():
+            # Use the username from the database if available
+            username_to_write = username if username != 'NONE' else (in_db if in_db != 'No' else '')
+            file.write(f"{name}\n@{username_to_write}\n.\n")
+        file.write(f"Total Artists: {total_artists}\nFound: {found}\nFound in DB: {found_in_db}\nNot Found: {not_found}\n")
+
+        if new_followed_artists:
+            file.write("\nNewly Followed Artists on Spotify:\n")
+            for artist in new_followed_artists:
+                file.write(f"{artist}\n")
+            file.write(f"Total Newly Followed: {len(new_followed_artists)}\n")
 
 def main():
     playlist_url = input("Enter the Spotify playlist URL: ")
@@ -84,14 +146,118 @@ def main():
     driver = webdriver.Chrome()
     try:
         instagram_accounts = scrape_artist_pages(driver, artists_info)
+        found_in_db = sum(1 for _, _, in_db in instagram_accounts if in_db != 'No')
         print(f"\nProcessed {len(artists_info)} artists.")
-        print(f"Found Instagram accounts for {len([acc for acc in instagram_accounts if acc[1] != 'NONE'])} artists.")
-        print(f"Instagram accounts not found for {len([acc for acc in instagram_accounts if acc[1] == 'NONE'])} artists.")
+        print(f"Found Instagram accounts for {found_in_db} artists.")
+        print(f"Instagram accounts not found for {len(artists_info) - found_in_db} artists.")
 
         create_csv = input("Would you like to save the results to a CSV file? (y/n): ").lower()
         if create_csv == 'y':
             save_to_csv(instagram_accounts)
             print("Instagram accounts saved to CSV file.")
+
+        create_text = input("Would you like to save the results to a text file? (y/n): ").lower()
+        if create_text == 'y':
+            new_artists = None
+            follow_artists = input("Would you like to follow the artists from this playlist on Spotify? (y/n): ").lower()
+            if follow_artists == 'y':
+                new_artists = follow_new_artists_in_playlist(playlist_url)
+                if new_artists:
+                    print("New artists followed on Spotify:")
+                    for artist in new_artists:
+                        print(artist)
+                else:
+                    print("You are already following all the artists in this playlist.")
+
+            save_to_text(instagram_accounts, new_artists)
+            print("Instagram accounts saved to text file.")
+    finally:
+        driver.quit()
+
+def get_all_followed_artists():
+    """Retrieve all artists the user is currently following."""
+    followed_artists = set()
+    results = sp.current_user_followed_artists(limit=50)
+
+    while results:
+        artists = results['artists']
+        for item in artists['items']:
+            followed_artists.add(item['id'])
+
+        if artists['next']:
+            results = sp.next(artists)
+        else:
+            break
+
+    return followed_artists
+
+def get_playlist_artist_ids(playlist_id):
+    """Retrieve all artist IDs from a playlist."""
+    artist_ids = set()
+    results = sp.playlist_tracks(playlist_id)
+
+    while results:
+        for item in results['items']:
+            track = item['track']
+            if track:
+                for artist in track['artists']:
+                    artist_ids.add(artist['id'])
+
+        if results['next']:
+            results = sp.next(results)
+        else:
+            break
+
+    return artist_ids
+
+def follow_new_artists_in_playlist(playlist_url):
+    playlist_id = playlist_url.split('/')[-1].split('?')[0]
+    followed_artist_ids = get_all_followed_artists()
+    playlist_artist_ids = get_playlist_artist_ids(playlist_id)
+
+    new_artists = []
+    for artist_id in playlist_artist_ids:
+        if artist_id not in followed_artist_ids:
+            sp.user_follow_artists([artist_id])
+            artist_info = sp.artist(artist_id)
+            new_artists.append(artist_info['name'])
+
+    return new_artists
+
+def main():
+    playlist_url = input("Enter the Spotify playlist URL: ")
+    artists_info = get_playlist_artists(sp, playlist_url)
+
+    driver = webdriver.Chrome()
+    try:
+        instagram_accounts = scrape_artist_pages(driver, artists_info)
+        found_in_db = sum(1 for _, _, in_db in instagram_accounts if in_db != 'No')
+        print(f"\nProcessed {len(artists_info)} artists.")
+        print(f"Found Instagram accounts for {found_in_db} artists.")
+        print(f"Instagram accounts not found for {len(artists_info) - found_in_db} artists.")
+
+        create_csv = input("Would you like to save the results to a CSV file? (y/n): ").lower()
+        if create_csv == 'y':
+            save_to_csv(instagram_accounts)
+            print("Instagram accounts saved to CSV file.")
+
+        create_text = input("Would you like to save the results to a text file? (y/n): ").lower()
+        if create_text == 'y':
+            save_to_text(instagram_accounts)
+            print("Instagram accounts saved to text file.")
+
+        follow_artists = input("Would you like to follow the artists from this playlist on Spotify? (y/n): ").lower()
+        if follow_artists == 'y':
+            new_artists = follow_new_artists_in_playlist(playlist_url)
+            if new_artists:
+                print("New artists followed on Spotify:")
+                for artist in new_artists:
+                    print(artist)
+            else:
+                print("You are already following all the artists in this playlist.")
+
+            # Save to text file including the new followed artists
+            save_to_text(instagram_accounts, new_artists)
     finally:
         driver.quit()
 
